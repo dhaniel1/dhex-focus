5ed3c9655ffd03a6721ead39af18e59c2eec3c3c
dhaniel1 <obioradaniel60@gmail.com>
2025-04-06 14:42:21 +0100

    refactor: link to commit 121406f5a93203402c3397ecdbb6ed6e89f3e1a9

--- src/components/Todos/index.ts
+++ src/components/Todos/index.ts
@@ -1,5 +1,6 @@
 export { default as TaskBoard } from "./taskBoard";
-export { default as TodoPreview } from "./inProgressPreview";
-export { default as TodoActions } from "./todoActions";
 export { default as TodoTablet } from "./todoTablet";
+export { default as TodoActions } from "./todoActions";
+export { default as TasksPreview } from "./tasksPreview";
+export { default as TasksInProgress } from "./tasksInProgress";
 export { default as TodoStageColumn } from "./todoStageColumn";
